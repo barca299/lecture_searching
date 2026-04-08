64d7670665dbbbda4411425983a6942625ee1b35
barca299 <barakubcova@seznam.cz>
2026-04-08 09:04:19 +0200

    x

--- searching.py
+++ searching.py
@@ -3,6 +3,15 @@ import json
 
 
 def read_data(file_name, field):
+    with open(file_name, "r") as file:
+        data = json.load(file)
+
+    if field not in data.keys():
+        return None
+    else:
+        return data[field]
+
+
     """
     Reads a JSON file and returns data for a given field.
 
@@ -24,8 +33,8 @@ def read_data(file_name, field):
 
 
 def main():
-    pass
-
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 if __name__ == "__main__":
     main()
