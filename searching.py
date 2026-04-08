1785b80232c627432f3c93624ad119a89f8a8584
barca299 <barakubcova@seznam.cz>
2026-04-08 09:31:12 +0200

    x

--- searching.py
+++ searching.py
@@ -30,11 +30,22 @@ def read_data(file_name, field):
     cwd_path = Path.cwd()
     
     file_path = cwd_path / file_name
+def linear_search(sequence, searched_number):
+    count = 0
+    seznam_pozic = []
+    for i, number in enumerate(sequence):
+        if searched_number == number:
+            seznam_pozic.append(i)
+            count += 1
+    slovnik = {"seznam_pozic": seznam_pozic, "count": count}
+    return slovnik
+
 
 
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
-
+    cislo = linear_search(sequential_data, 5)
+    print(cislo)
 if __name__ == "__main__":
     main()
